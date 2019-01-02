2cfdccb1d1a0e810086010b33061d62852bf9636
Jason Stephen <jason@jasons-macbook-pro.local>
2019-01-01 23:02:16 -0600

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Jason Stephen on 1/1/19.
+//  Copyright © 2019 Jason Stephen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
